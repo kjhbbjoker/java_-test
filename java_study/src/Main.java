2714ddacb68fdd0a7fc9a6d75f87cf2f227e5e55
jonghun <whdgns4288@naver.com>
2023-07-18 23:07:14 +0900

    작성완료

---
+++ java_study/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("드디어 시작");
+        System.out.println("이거는 되나?");
+        System.out.println("이렇게?");
+        System.out.println("ddddd");
+    }
+}
