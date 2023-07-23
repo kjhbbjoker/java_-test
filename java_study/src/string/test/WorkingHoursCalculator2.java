ed3ef73398d55daeb247ba3c05df265cde682bf6
jonghun <whdgns4288@naver.com>
2023-07-23 21:05:04 +0900

    점심시간 포함

--- java_study/src/string/test/WorkingHoursCalculator2.java
+++ java_study/src/string/test/WorkingHoursCalculator2.java
@@ -2,32 +2,34 @@ package string.test;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
-public class WorkingHoursCalculator {
+public class WorkingHoursCalculator2 {
 
     private static final int WORK_START_HOUR = 12;
     private static final int WORK_END_HOUR = 18;
+    private static final int LUNCH_START_HOUR = 13;
+    private static final int LUNCH_END_HOUR = 14;
     private static final int SECONDS_PER_HOUR = 3600;
 
     public static void main(String[] args) {
-        int totalWorkDurationInSeconds = 20 * SECONDS_PER_HOUR;
-        String result = calculateWorkingHours(36000);
+        int totalWorkDurationInSeconds = 10 * SECONDS_PER_HOUR;
+        String result = calculateWorkingHours(totalWorkDurationInSeconds);
         System.out.println("Work duration will be completed by: " + result);
     }
 
     public static String calculateWorkingHours(int totalWorkDurationInSeconds) {
-        //총 근무시간  끝나는 시간 - 시작 시간
+        // Calculate the total lunch break time in seconds
+        int lunchBreakInSeconds = (LUNCH_END_HOUR - LUNCH_START_HOUR) * SECONDS_PER_HOUR;
+
+        // Deduct the lunch break time from the total work duration
+        totalWorkDurationInSeconds -= lunchBreakInSeconds;
+
         int workHoursPerDay = WORK_END_HOUR - WORK_START_HOUR;
 
-
         int totalWorkDays = totalWorkDurationInSeconds / (workHoursPerDay * SECONDS_PER_HOUR);
-
-        //일수 남은 시간
         int remainingSeconds = totalWorkDurationInSeconds % (workHoursPerDay * SECONDS_PER_HOUR);
 
         int remainingHours = remainingSeconds / SECONDS_PER_HOUR;
-
         remainingSeconds %= SECONDS_PER_HOUR;
 
         int remainingMinutes = remainingSeconds / 60;
